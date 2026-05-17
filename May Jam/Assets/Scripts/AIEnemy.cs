a223a5c34e30a15213b117aac2fa9c2d55980fc4
Maryeva Gonzalez <144571162+meva08@users.noreply.github.com>
2026-05-17 01:37:55 -0500

    Enemy animation and movement

--- May Jam/Assets/Scripts/AIEnemy.cs
+++ May Jam/Assets/Scripts/AIEnemy.cs
@@ -2,32 +2,49 @@ using UnityEngine;
 
 public class AIEnemy : MonoBehaviour
 {
+    [SerializeField] private float lightDrainPerSecond = 0.5f;
     public Transform player;
     private PlayerLight playerLight;
     public float speed;
     private float distance;
     public float distanceBetween;
+    private Animator animator;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         player = GameObject.FindWithTag("Player").transform;
         playerLight = player.GetComponent<PlayerLight>();
+        animator = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
     void Update()
     {
         if (player == null) return;
 
         distance = Vector2.Distance(transform.position, player.position);
         Vector2 direction = (player.position - transform.position).normalized;
-        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
 
         if (distance < playerLight.GetCurrentRadius() + distanceBetween)
         {
             transform.position = Vector2.MoveTowards(transform.position, player.position, speed * Time.deltaTime);
-            transform.rotation = Quaternion.Euler(Vector3.forward * angle);
+            
+            // Update animator
+            Vector2 velocity = (Vector2)player.position - (Vector2)transform.position;
+            animator.SetFloat("Speed", velocity.magnitude);
+            animator.SetFloat("DirectionX", direction.x);
+            animator.SetFloat("DirectionY", direction.y);
+        }
+        else
+        {
+            animator.SetFloat("Speed", 0f);
+        }
+    }
+
+    void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerLight.ReduceRadius(lightDrainPerSecond * Time.deltaTime);
         }
     }
 }
